b052d6297d172f70440437b777b3edbbb2b10c03
lijinchuan <403479851@qq.com>
2016-07-22 13:38:26 +0800

    增加图片裁剪方法

--- LJC.FrameWork/Comm/ImageHelper.cs
+++ LJC.FrameWork/Comm/ImageHelper.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Windows.Forms;
 using System.IO;
 using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
 
 namespace LJC.FrameWork.Comm
 {
@@ -73,6 +74,7 @@ namespace LJC.FrameWork.Comm
             {
                 Image bmp = Image.FromFile(bmpPath);
                 bmp.Save(savePath, System.Drawing.Imaging.ImageFormat.Jpeg);
+
                 return true;
             }
             catch (Exception e)
@@ -122,6 +124,63 @@ namespace LJC.FrameWork.Comm
             {
                 return false;
             }
+        }
+
+        /// <summary>  
+        ///  Resize图片   
+        /// </summary>  
+        /// <param name="bmp">原始Bitmap </param>  
+        /// <param name="newW">新的宽度</param>  
+        /// <param name="newH">新的高度</param>  
+        /// <param name="Mode">保留着，暂时未用</param>  
+        /// <returns>处理以后的图片</returns>  
+
+        public static Bitmap ResizeImage(Bitmap bmp, int newW, int newH, int Mode)
+        {
+            Bitmap b = new Bitmap(newW, newH);
+            Graphics g = Graphics.FromImage(b);
+            // 插值算法的质量   
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            g.DrawImage(bmp, new Rectangle(0, 0, newW, newH), new Rectangle(0, 0, bmp.Width, bmp.Height), GraphicsUnit.Pixel);
+            g.Dispose();
+            return b;
+        }
+        /// <summary>  
+        /// 剪裁 -- 用GDI+   
+        /// </summary>  
+        /// <param name="b">原始Bitmap</param>  
+        /// <param name="StartX">开始坐标X</param>  
+        /// <param name="StartY">开始坐标Y</param>  
+        /// <param name="iWidth">宽度</param>  
+        /// <param name="iHeight">高度</param>  
+        /// <returns>剪裁后的Bitmap</returns>  
+        public static Bitmap Cut(Bitmap b, int StartX, int StartY, int iWidth, int iHeight)
+        {
+            if (b == null)
+            {
+                return null;
+            }
+            int w = b.Width;
+            int h = b.Height;
+            if (StartX >= w || StartY >= h)
+            {
+                return null;
+            }
+            if (StartX + iWidth > w)
+            {
+                iWidth = w - StartX;
+            }
+            if (StartY + iHeight > h)
+            {
+                iHeight = h - StartY;
+            }
+
+            Bitmap bmpOut = new Bitmap(iWidth, iHeight, PixelFormat.Format24bppRgb);
+            Graphics g = Graphics.FromImage(bmpOut);
+            g.DrawImage(b, new Rectangle(0, 0, iWidth, iHeight), new Rectangle(StartX, StartY, iWidth, iHeight), GraphicsUnit.Pixel);
+            g.Dispose();
+            return bmpOut;
+
         }
     }
 }
