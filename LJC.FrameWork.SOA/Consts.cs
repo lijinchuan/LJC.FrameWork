f84947f684fb636188a984618ce77d75de449538
lijinchuan <403479851@qq.com>
2017-07-04 22:53:33 +0800

    提交文件

---
+++ LJC.FrameWork.SOA/Consts.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LJC.FrameWork.SOA
+{
+    public static class Consts
+    {
+        public const int ESBServerServiceNo = 0;
+        public const int FunNo_ECHO = 1;
+        public const int FunNo_Environment = 2;
+        public const int FunNo_ExistsAServiceNo = 3;
+    }
+}
