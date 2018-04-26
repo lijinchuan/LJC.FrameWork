829eb5ffacc4557c3b98d7eb6a1e8a622410995f
lijinchuan <403479851@qq.com>
2018-04-26 09:16:06 +0800

    CouchbaseHelper优化

--- LJC.FrameWork.Couchbase/CouchbaseHelper.cs
+++ LJC.FrameWork.Couchbase/CouchbaseHelper.cs
@@ -54,6 +54,33 @@ namespace LJC.FrameWork.Couchbase
             return client;
         }
 
+        public static CB.CouchbaseClient GetClient(string serverip,int port, string bucket)
+        {
+            CB.CouchbaseClient client = null;
+            string key = serverip + bucket;
+            if (!ClientDic.TryGetValue(key, out client))
+            {
+                if (string.IsNullOrWhiteSpace(key))
+                {
+                    throw new ArgumentNullException("serverip&bucket");
+                }
+
+                CB.Configuration.CouchbaseClientConfiguration config = new CB.Configuration.CouchbaseClientConfiguration();
+                if (!string.IsNullOrWhiteSpace(bucket))
+                {
+                    config.Bucket = bucket;
+                }
+                config.SocketPool.MaxPoolSize = 10;
+                config.SocketPool.MinPoolSize = 5;
+                config.Urls.Add(new Uri(string.Format("http://{0}:{1}/pools", serverip,port)));
+                client = new CB.CouchbaseClient(config);
+
+                ClientDic.TryAdd(key, client);
+            }
+
+            return client;
+        }
+
         public static bool Store(this CB.CouchbaseClient client, StoreMode storemode, string key, object value)
         {
             return client.Store((Enyim.Caching.Memcached.StoreMode)storemode, key, value);
