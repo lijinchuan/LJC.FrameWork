789d590c8307b5bdbf8b92a7166ca25683865afc
lijinchuan <403479851@qq.com>
2021-04-11 21:44:23 +0800

    解释器优化

---
+++ LJC.FrameWork/CodeExpression/CompareHelper.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LJC.FrameWork.CodeExpression
+{
+    static class CompareHelper
+    {
+        public static bool Eq(this object obj1,object obj2)
+        {
+            if (obj1 == null && obj2 == null)
+            {
+                return true;
+            }
+
+            if (obj1 == null || obj2 == null)
+            {
+                return false;
+            }
+
+            if(obj1 is double)
+            {
+                return (double)obj1 == Convert.ToDouble(obj2);
+            }
+
+            if(obj1 is DateTime)
+            {
+                return (DateTime)obj1 == Convert.ToDateTime(obj2);
+            }
+
+            if(obj1 is string)
+            {
+                return (string)obj1 == Convert.ToString(obj2);
+            }
+
+            return false;
+        }
+
+        public static bool BigerEq(this object obj1,object obj2)
+        {
+            if (obj1 == null && obj2 == null)
+            {
+                return true;
+            }
+
+            if (obj1 == null)
+            {
+                return false;
+            }
+
+            if (obj2 == null)
+            {
+                return true;
+            }
+
+            if (obj1 is double)
+            {
+                return (double)obj1 >= Convert.ToDouble(obj2);
+            }
+
+            if (obj1 is DateTime)
+            {
+                return (DateTime)obj1 >= Convert.ToDateTime(obj2);
+            }
+
+            if (obj1 is string)
+            {
+                return Convert.ToString(obj1).CompareTo(Convert.ToString(obj2))>=0;
+            }
+
+            return false;
+        }
+
+        public static bool Biger(this object obj1, object obj2)
+        {
+            if (obj1 == null && obj2 == null)
+            {
+                return false;
+            }
+
+            if (obj1 == null)
+            {
+                return false;
+            }
+
+            if (obj2 == null)
+            {
+                return true;
+            }
+
+            if (obj1 is double)
+            {
+                return (double)obj1 > Convert.ToDouble(obj2);
+            }
+
+            if (obj1 is DateTime)
+            {
+                return (DateTime)obj1 > Convert.ToDateTime(obj2);
+            }
+
+            if (obj1 is string)
+            {
+                return Convert.ToString(obj1).CompareTo(Convert.ToString(obj2)) > 0;
+            }
+
+            return false;
+        }
+
+        public static bool SmallerEq(this object obj1, object obj2)
+        {
+            if (obj1 == null && obj2 == null)
+            {
+                return true;
+            }
+
+            if (obj1 == null)
+            {
+                return true;
+            }
+
+            if (obj2 == null)
+            {
+                return false;
+            }
+
+            if (obj1 is double)
+            {
+                return (double)obj1 <= Convert.ToDouble(obj2);
+            }
+
+            if (obj1 is DateTime)
+            {
+                return (DateTime)obj1 <= Convert.ToDateTime(obj2);
+            }
+
+            if (obj1 is string)
+            {
+                return Convert.ToString(obj1).CompareTo(Convert.ToString(obj2)) <= 0;
+            }
+
+            return false;
+        }
+
+        public static bool Smaller(this object obj1, object obj2)
+        {
+            if (obj1 == null && obj2 == null)
+            {
+                return false;
+            }
+
+            if (obj1 == null)
+            {
+                return true;
+            }
+
+            if (obj2 == null)
+            {
+                return false;
+            }
+
+            if (obj1 is double)
+            {
+                return (double)obj1 < Convert.ToDouble(obj2);
+            }
+
+            if (obj1 is DateTime)
+            {
+                return (DateTime)obj1 < Convert.ToDateTime(obj2);
+            }
+
+            if (obj1 is string)
+            {
+                return Convert.ToString(obj1).CompareTo(Convert.ToString(obj2)) < 0;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ LJC.FrameWork/CodeExpression/SystemFunction/Dat.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LJC.FrameWork.CodeExpression.SystemFunction
+{
+    internal class Dat: SysFun
+    {
+        public override string SignName => "Dat";
+
+        public override int Params => 1;
+
+        protected override CalResult SingOperate()
+        {
+            var param1 = GetPara(0);
+
+            return new CalResult
+            {
+                Result=Convert.ToDateTime(param1).Date,
+                ResultType=typeof(DateTime)
+            };
+        }
+
+        protected override CalResult CollectOperate()
+        {
+            var param1 = GetPara(0);
+
+            var arr = param1.ToArr();
+
+            return new CalResult
+            {
+                Results = arr.Select(p => (object)Convert.ToDateTime(p).Date).ToArray(),
+                ResultType = typeof(DateTime[])
+            };
+        }
+    }
+}
